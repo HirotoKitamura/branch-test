4ba9d4ca5dcc24f976c6265aa310b976419bc242
mitsu-mitsui <yuiko.mitsui@rakus.co.jp>
2020-07-09 11:34:00 +0900

    スピードを追加実装

--- src/main/java/com/example/demo/domain/Car.java
+++ src/main/java/com/example/demo/domain/Car.java
@@ -1,5 +1,14 @@
 package com.example.demo.domain;
 
 public class Car {
+	private int speed = 100;
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
 
 }
